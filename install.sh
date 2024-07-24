f7169c9200c5bb52592b29c58ad3c52184ed2988
trombiano1 <trombiano1@gmail.com>
2024-07-24 17:19:32 +0900

    add: install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Install.
+sudo apt install curl stow zsh tmux vim
+
+# Clone dotfiles repo.
+cd ~
+git clone --recurse-submodules -j8 https://github.com/massanf/dotfiles.git
+cd ~/dotfiles
+
+# Stow.
+stow vim zsh tmux
+
+# Simlinks.
+ln -s ~/dotfiles/zsh/.oh-my-zsh-powerlevel10k ~/dotfiles/zsh/.oh-my-zsh/themes/powerlevel10k
+
