74872d6f9a47b2cd95e3e2b2c3a338c612c65fcb
trombiano1 <trombiano1@gmail.com>
2024-07-31 19:43:25 +0900

    fix fzf path

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/mfujita/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/mfujita/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 source <(fzf --zsh)
